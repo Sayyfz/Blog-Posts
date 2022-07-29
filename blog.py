2df0c0f5608df86306825969b777c2af6cafd1e3
Sayyf <seif_fire2@hotmail.com>
2022-07-30 01:06:07 +0200

    Register & Login Forms done

--- blog.py
+++ blog.py
@@ -1,6 +1,9 @@
-from flask import Flask,render_template,request
+from flask import Flask, flash,redirect
+from flask import Flask,render_template,request,url_for
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = 'cffd88e7a1063b2a76e356cc59551da0'
 
 posts = [
     {
@@ -14,17 +17,95 @@ posts = [
         'title': 'Post2',
         'content': 'Second Post Content',
         'date_posted': 'March 4, 2018'
+    } ,
+    {
+        'author': 'Seif Ashraf',
+        'title': 'Post2',
+        'content': 'Second Post Content',
+        'date_posted': 'March 4, 2018'
+    } ,
+    {
+        'author': 'Seif Ashraf',
+        'title': 'Post2',
+        'content': 'Second Post Content',
+        'date_posted': 'March 4, 2018'
+    } ,
+    {
+        'author': 'Seif Ashraf',
+        'title': 'Post2',
+        'content': 'Second Post Content',
+        'date_posted': 'March 4, 2018'
+    } ,
+    {
+        'author': 'Seif Ashraf',
+        'title': 'Post2',
+        'content': 'Second Post Content',
+        'date_posted': 'March 4, 2018'
+    } ,
+    {
+        'author': 'Seif Ashraf',
+        'title': 'Post2',
+        'content': 'Second Post Content',
+        'date_posted': 'March 4, 2018'
+    } ,
+    {
+        'author': 'Seif Ashraf',
+        'title': 'Post2',
+        'content': 'Second Post Content',
+        'date_posted': 'March 4, 2018'
+    } ,
+    {
+        'author': 'Seif Ashraf',
+        'title': 'Post2',
+        'content': 'Second Post Content',
+        'date_posted': 'March 4, 2018'
+    } ,
+    {
+        'author': 'Seif Ashraf',
+        'title': 'Post2',
+        'content': 'Second Post Content',
+        'date_posted': 'March 4, 2018'
+    } ,
+    {
+        'author': 'Seif Ashraf',
+        'title': 'Post2',
+        'content': 'Second Post Content',
+        'date_posted': 'March 4, 2018'
     } 
+    
+    
 ]
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template("home.html", posts = posts)
+    return render_template('home.html', posts = posts)
 
 @app.route("/about")
 def about():
-    return render_template("about.html", title = 'About')
+    return render_template('about.html', title = 'About')
+
+
+@app.route("/register", methods = ['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account Created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html',title='Register',  form = form)
+
+@app.route("/login", methods = ['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login unsuccessful. Please check username and password', 'danger')
+            
+    return render_template('login.html',title='Login',  form = form)
+
 
 if __name__ == '__main__':
      app.run(debug = True)
